1886f21c7e7b2044dd28f93b3e2d12068f1884c4
LeeamShutzman <leeamshutzman@gmail.com>
2022-06-30 17:25:20 -0500

    Create operations added

--- src/main/java/org/fabm/backend/profile/controller/ProfileController.java
+++ src/main/java/org/fabm/backend/profile/controller/ProfileController.java
@@ -2,16 +2,9 @@ package org.fabm.backend.profile.controller;
 
 import org.fabm.backend.profile.model.Photo;
 import org.fabm.backend.profile.model.Song;
-import org.fabm.backend.profile.repository.PhotoRepository;
-import org.fabm.backend.profile.repository.SongRepository;
 import org.fabm.backend.profile.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,12 +15,44 @@ public class ProfileController {
     @Autowired
     private ProfileService profileService;
 
-    @GetMapping("getSongs")
+    /*****************************************************************************/
+    // Song CRUD operations
+
+    // Add a single Song object
+    @PostMapping("addSong") // localhost:5001/profiles/addSong
+    public Song addSong(@RequestBody Song song){
+        return profileService.addSong(song);
+    }
+
+    // Add multiple Song objects
+    @PostMapping("addSongs") // localhost:5001/profiles/addSongs
+    public List<Song> addSongs(@RequestBody List<Song> songList){
+        return profileService.addSongs(songList);
+    }
+
+    // Returns a list of Song objects grouped by user
+    @GetMapping("getSongs") // localhost:5001/profiles/getSongs?userId=#
     public List<Song> getSongsByUser(@RequestParam long userId){
         return profileService.getSongsByUser(userId);
     }
 
-    @GetMapping("getPhotos")
+    /*****************************************************************************/
+    // Photo CRUD operations
+
+    // Add a single Photo object
+    @PostMapping("addPhoto") // localhost:5001/profiles/addPhoto
+    public Photo addPhoto(@RequestBody Photo photo){
+        return profileService.addPhoto(photo);
+    }
+
+    // Add multiple Photo objects
+    @PostMapping("addPhotos") // localhost:5001/profiles/addPhoto
+    public List<Photo> addPhotos(@RequestBody List<Photo> photoList){
+        return profileService.addPhotos(photoList);
+    }
+
+    // Returns a list of Photo objects grouped by user
+    @GetMapping("getPhotos") // localhost:5001/profiles/getPhotos?userId=#
     public List<Photo> getPhotosByUser(@RequestParam long userId){
         return profileService.getPhotosByUser(userId);
     }
